d6aa334bcae532f31a7d9e1d01f8f94102ee28c8
cmhanner <chanceforcode@gmail.com>
2025-10-21 12:08:48 -0400

    Per Class Example 10/21 Showing Home and About on Page with different styles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/index.css';
+import Home from './pages/Home'; //  pulling from line 4
+import About from './pages/About';
 // import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <h1>Hi Chance</h1>
-    <h2>Loving this</h2>
+    <h2>Loving This</h2>
+    <Home/>
+    <About/>
   </React.StrictMode>
 );
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import "./../css/Home.css"
+
+const Home = () => {
+    return (
+        <div id = "home">
+            <h1>I'm Home</h1>
+            <h2>Home is for the heart</h2>
+        </div>
+
+    );
+};
+
+export default Home;
